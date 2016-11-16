52ce05648ea4c5100dce9970255c5b1a0966e0ae
Suan-Aik Yeo <suan@usertesting.com>
2016-11-16 12:11:15 -0500

    only raise first 200 chars of errors to prevent flooding

--- lib/biscuit.rb
+++ lib/biscuit.rb
@@ -3,7 +3,7 @@ require "biscuit/version"
 module Biscuit
   def self.run!(command)
     result = `#{__dir__}/../bin/_biscuit #{command}`
-    raise(result) unless $?.success?
+    raise(result.slice(0, 200)) unless $?.success?
     result
   end
 end
