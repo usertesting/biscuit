434ab7a20aff2898cd1a149bd32ecd1d9b25e3b7
Perry <perryism@gmail.com>
2018-03-30 11:55:01 -0700

    Add spec for biscuit

--- lib/biscuit.rb
+++ lib/biscuit.rb
@@ -1,9 +1,10 @@
 require "biscuit/version"
+require "open3"
 
 module Biscuit
   def self.run!(command)
-    result = `#{__dir__}/../bin/_biscuit #{command}`
-    raise(result.slice(0, 200)) unless $?.success?
-    result
+    stdout, stderr, status = Open3.capture3("#{__dir__}/../bin/_biscuit #{command}")
+    raise(stderr.slice(0, 200)) unless status == 0
+    stdout
   end
 end
